73210b308d92cacb4ce8770fde6921c7d751f559
Kalinin Yakov <z-chutvishechem@mail.ru>
2021-12-09 18:52:39 +0700

    Indexchanges

--- src/BMIService.java
+++ src/BMIService.java
@@ -1,7 +1,7 @@
 public class BMIService {
     public int calculate (int growth, int mass) {
-        int growthindex = growth * growth;
-        int index = mass * 10000 / growthindex;
+        int index1 = growth * growth;
+        int index = mass * 10000 / index1;
         return index;
     }
 
